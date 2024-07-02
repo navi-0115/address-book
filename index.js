d4b85bb23092026454380400810b12d77a9a418b
Raden-nvii <navinugraha15@gmail.com>
2024-07-02 09:56:08 +0700

    feat: refine js variables

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
-let label = [
-  { id: "1", labelName: "Friend" },
-  { id: "2", labelName: "Family" },
+const labels = [
+  { id: 1, name: "Friend" },
+  { id: 2, name: "Family" },
 ];
 
-let contact = [
+const contacts = [
   {
-    id: "1",
+    id: 1,
     fullName: "Alaska Bandar Salju",
     email: "alaska@mail.com",
     phoneNumber: "62888092131232",
     address: "Jl. Bikini Bottom",
     jobTitle: "CEO",
-    label: [0],
+    labels: [labels[0]],
   },
   {
-    id: "2",
+    id: 2,
     fullName: "Blue Adams",
     email: "adams@mail.com",
     phoneNumber: "62888099912323",
     address: "Jl. Pasific Ocean",
     jobTitle: "Cleaner",
-    label: [0],
+    labels: [labels[0]],
   },
   {
-    id: "3",
+    id: 3,
     fullName: "Owen Hadi",
     email: "owenhadi@mail.com",
     phoneNumber: "6288809990023",
     address: "Jl. Merdeka Raya",
     jobTitle: "Software Engineer",
-    label: [1],
+    label: [labels[0]],
   },
 ];
-
-console.log([label, contact]);
+console.log([labels, contacts]);
