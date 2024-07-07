0300a8506006ba19cf122a83338e4700447eeb0c
Raden-nvii <navinugraha15@gmail.com>
2024-07-07 09:39:36 +0700

    feat: erase contacts array

--- index.js
+++ index.js
@@ -3,35 +3,7 @@ const labels = [
   { id: 2, name: "Family" },
 ];
 
-const contacts = [
-  {
-    id: 1,
-    fullName: "Alaska Bandar Salju",
-    email: "alaska@mail.com",
-    phoneNumber: "62888092131232",
-    address: "Sleman, DIY",
-    jobTitle: "CEO",
-    labels: ["Family", "Colleague"],
-  },
-  {
-    id: 2,
-    fullName: "Blue Adams",
-    email: "adams@mail.com",
-    phoneNumber: "62888099912323",
-    address: "Sleman, DIY",
-    jobTitle: "Cleaner",
-    labels: ["Colleague"],
-  },
-  {
-    id: 3,
-    fullName: "Owen Hadi",
-    email: "owenhadi@mail.com",
-    phoneNumber: "6288809990023",
-    address: "Surabaya, Jawa Timur",
-    jobTitle: "Software Engineer",
-    labels: ["Others"],
-  },
-];
+const contacts = [];
 
 // Render Contact
 function renderContacts(contacts) {
