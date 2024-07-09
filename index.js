80fd55cb93098b48eb9d7c23cd5cdb832f0143a7
Raden-nvii <navinugraha15@gmail.com>
2024-07-09 17:21:26 +0700

    feat: add update and delete modal UI

--- index.js
+++ index.js
@@ -37,9 +37,31 @@ const renderContacts = (specificRenderContacts) => {
         <td class="px-4 py-3">${contact.phone}</td>
         <td class="px-4 py-3">${contact.jobTitle}</td>
         <td class="px-4 py-3">${contact.address}</td>
-        <td class="px-4 py-3">
-          <button class="edit-contact bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Edit</button>
-          <button class="delete-contact bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete</button>
+        <td class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            <div class="flex items-center space-x-2">
+                <button type="button" data-drawer-target="drawer-update-product" data-drawer-show="drawer-update-product"
+                    aria-controls="drawer-update-product"
+                    class="py-2 px-3 flex items-center text-sm font-medium text-center text-black bg-black rounded-lg hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2 -ml-0.5" viewbox="0 0 20 20" fill="currentColor"
+                        aria-hidden="true">
+                        <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+                        <path fill-rule="evenodd"
+                            d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+                            clip-rule="evenodd" />
+                    </svg>
+                    Edit
+                </button>
+                <button type="button" data-modal-target="delete-modal" data-modal-toggle="delete-modal"
+                    class="flex items-center text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1 -ml-0.5" viewbox="0 0 20 20" fill="currentColor"
+                        aria-hidden="true">
+                        <path fill-rule="evenodd"
+                            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                            clip-rule="evenodd" />
+                    </svg>
+                    Delete
+                </button>
+            </div>
         </td>
     `;
     contactList.appendChild(contactItem);
@@ -63,12 +85,12 @@ const searchContacts = (query) => {
   return filtered;
 };
 
-const deleteContact = (id) => {
-  const contacts = getContacts();
-  const removeContact = contacts.filter((contact) => contact.id !== id);
-  saveContacts(removeContact);
-  return removeContact;
-};
+// const deleteContact = (id) => {
+//   const contacts = getContacts();
+//   const removeContact = contacts.filter((contact) => contact.id !== id);
+//   saveContacts(removeContact);
+//   return removeContact;
+// };
 
 // function to hide contact modal
 const hideContactModal = () => {
@@ -78,11 +100,11 @@ const hideContactModal = () => {
 };
 
 // Event listener for delete contact form
-deleteModal.addEventListener("submit", (event) => {
-  const contacts = getContacts();
-  const deletedContact = deleteContact(contacts);
-  renderContacts(deletedContact);
-});
+// deleteModal.addEventListener("submit", (event) => {
+//   const contacts = getContacts();
+//   const deletedContact = deleteContact(contacts);
+//   renderContacts(deletedContact);
+// });
 
 // Event Listener for Add Contact Form
 contactForm.addEventListener("submit", (event) => {
